1ee4773dd04e4e3d3902009e1a2e12d12e02afdf
Prateek Mitra <38394892+enzi077@users.noreply.github.com>
2020-10-26 02:18:28 +0530

    dashboard graph created

--- src/App.js
+++ src/App.js
@@ -1,89 +1,34 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
-import randomColor from './utils/randomColor'
-import {Line} from 'react-chartjs-2';
+import DashboardGraph from './components/DashboardGraph';
 
 function App() {
-    const [data,setData]=useState([])
     const [countriesData,setCountriesData]=useState([])
-    const tempCountry=['USA','India','Brazil','Russia','Spain']
-    const fatalityRateData=[]
+    const tempCountry=[]
     const lastDays=100
-    //const subdata={datasets:[]}
     
     useEffect(()=>{
-        reqData()
+        reqDataForTopCountries()
     },[])
     
-    // const reqDataForTopCountries=async()=>{
-    //     const responseCountry=await fetch('https://disease.sh/v3/covid-19/countries?sort=cases')
-    //     const dataCountry=await responseCountry.json()
-    //     setCountriesData(dataCountry)
-    //     countriesData.forEach((countryData,index)=>{
-    //         if(index<5){
-    //             tempCountry.push(countryData.country)
-    //         }
-            
-    //     })
-    // }
-    
-    const reqData=async()=>{
-        const response=await 
-            fetch(`https://disease.sh/v3/covid-19/historical/${tempCountry[0]}%2C%20${tempCountry[1]}%2C%20${tempCountry[2]}%2C%20${tempCountry[3]}%2C%20${tempCountry[4]}?lastdays=${lastDays}`)
-        const data=await response.json()
-        console.log(data);
-        setData(data)
+    const reqDataForTopCountries=async()=>{
+        const responseCountry=await fetch('https://disease.sh/v3/covid-19/countries?sort=cases')
+        const dataCountry=await responseCountry.json()
+        setCountriesData(dataCountry)
     }
     
-    const genFatalityRateData=(countryName,fatalityRate,date)=>{
-        fatalityRateData.push({countryName, fatalityRate, date})
-        return fatalityRateData
-    }
-    
-    const createNewDataset=(fatalityRateData)=>{
-        const subdata={labels:[],datasets:[]}
-        //label: fatalityRateData[i].countryName 
-        for(let i =0;i<fatalityRateData.length;i=i+lastDays){
-            let nthObject = {label: fatalityRateData[i].countryName,data:[],fill: false,borderColor: randomColor()};
-            let fatalityRateArray = []
-            for(let j=i;j<i+lastDays;j++){
-                if(j<fatalityRateData.length){
-                    fatalityRateArray.push(fatalityRateData[j].fatalityRate)
-                }
-                if (j < lastDays) {
-                    subdata.labels.push(fatalityRateData[j].date);
-                }
-            }
-            
-            nthObject.data = fatalityRateArray
-            subdata.datasets.push(nthObject);
-        }
-        return subdata
-    }
-    
-  return (
-    <div className="App">
-        {
-            data.forEach((country)=>{
-                let cases=country.timeline.cases
-                let deaths=country.timeline.deaths
-                for(let i in cases){
-                    for(let j in deaths){
-                        if(i===j){
-                            let fatalityRate=(deaths[j])/(cases[i])
-                            genFatalityRateData(country.country,fatalityRate,i)
-                        }
+    return(
+        <div>
+            {
+                countriesData.forEach((country,index)=>{
+                    if(index<5){
+                        tempCountry.push(country.country)
                     }
-                }
-            })
-        }
-        {
-            <div>
-                <Line data={createNewDataset(fatalityRateData)}/>
-            </div>
-        }
-    </div>
-  );
+                })
+            }
+            <DashboardGraph countries={tempCountry} lastDays={lastDays}/>
+        </div>
+    )
 }
 
 export default App;
--- src/components/DashboardGraph.js
+++ src/components/DashboardGraph.js
@@ -1,11 +1,76 @@
-import React from 'react'
+import React, { useEffect,useState } from 'react'
 import randomColor from '../utils/randomColor'
 import {Line} from 'react-chartjs-2';
 
-const DashboardGraph=()=>{
+const DashboardGraph=({countries,lastDays})=>{
+    const [data,setData]=useState([])
+    const fatalityRateData=[]
+    
+    useEffect(()=>{
+        reqData()
+    },[countries])
+    
+    const reqData=async()=>{
+        const response=await 
+            fetch(`https://disease.sh/v3/covid-19/historical/${countries[0]}%2C${countries[1]}%2C${countries[2]}%2C${countries[3]}%2C${countries[4]}?lastdays=${lastDays}`)
+        const dataFromApi=await response.json()
+        setData(dataFromApi)
+    }
+    
+    const genFatalityRateData=(countryName,fatalityRate,date)=>{
+        fatalityRateData.push({countryName, fatalityRate, date})
+        return fatalityRateData
+    }
+    
+    const createNewDataset=(fatalityRateData)=>{
+        const subdata={labels:[],datasets:[]}
+        for(let i =0;i<fatalityRateData.length;i=i+lastDays){
+            let nthObject = {label: fatalityRateData[i].countryName,data:[],fill: false,borderColor: randomColor(),pointRadius:0.9};
+            let fatalityRateArray = []
+            for(let j=i;j<i+lastDays;j++){
+                if(j<fatalityRateData.length){
+                    fatalityRateArray.push(fatalityRateData[j].fatalityRate)
+                }
+                if (j < lastDays) {
+                    subdata.labels.push(fatalityRateData[j].date);
+                }
+            }
+            
+            nthObject.data = fatalityRateArray
+            subdata.datasets.push(nthObject);
+        }
+        return subdata
+    }
+    
+    const options={
+        responsive: true,
+    }
+    
     return (
         <div>
+            {data.forEach(country=>{
+                if(country.message!=="Country not found or doesn't have any historical data"){
+                    let cases=country.timeline.cases
+                    let deaths=country.timeline.deaths
+                    for(let i in cases){
+                        for(let j in deaths){
+                            if(i===j){
+                                let fatalityRate=(deaths[j])/(cases[i])
+                                genFatalityRateData(country.country,fatalityRate,i)
+                            }
+                        }
+                    }
+                }
+            })}
             
+            {
+                <div>
+                    <Line 
+                        data={createNewDataset(fatalityRateData)}
+                        options={options}
+                    />
+                </div>
+            }
         </div>
     )
 }
